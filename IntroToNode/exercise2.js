0c79e264df49640a0ec03a6c9b0330940b2c8400
Yuying He <Rene-Hatsune@users.noreply.github.com>
2019-04-05 14:49:27 -0400

    Add intro

---
+++ IntroToNode/exercise2.js
@@ -0,0 +1,23 @@
+//Node Exercise 2
+//Average grade
+
+//create a file, "grader.js"
+
+//In the file define a new function named "average"
+//It should take a single parameter: an array of test scores(all numbers)
+//It should return the average score in the array, rounded to the nearest whole number
+
+function average(scores){
+    var total = 0;
+    scores.forEach(function (score){
+        total += score;
+    });
+    var avg = total/scores.length;
+    return Math.round(avg);
+}
+
+var scores = [90, 98, 89, 100, 100, 86, 94];
+console.log(average(scores));//should return 94
+
+var scores2 = [40, 65, 77, 82, 80, 54, 73, 63, 95, 49];
+console.log(average(scores2));//should return 68
---
+++ IntroToNode/objectives.md
@@ -0,0 +1,14 @@
+#Intro to Node
+
+* What is Node?
+* Why use Node?
+* It doesn't matter.
+
+* Interact with node console
+* run a file with node
+* 
+* Node echo exercise
+
+* Node average exercise
+
+* npm Faker Exercise
